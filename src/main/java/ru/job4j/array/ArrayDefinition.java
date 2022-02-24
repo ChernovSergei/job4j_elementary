9a2272a998622c8488d07c6812538ec78466bf9e
ChernovSergei <sergey.v.chernov@yandex.ru>
2022-02-24 08:37:20 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,9 +5,20 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+
+        names[0] = "Vanya";
+        names[1] = "Ibrahim";
+        names[2] = "Osaka";
+        names[3] = "Mike";
 
         System.out.println("Ages array size = " + ages.length);
         System.out.println("Surnames array size = " + surnames.length);
         System.out.println("Prices array size = " + prices.length);
+        System.out.println();
+
+        for (String name: names) {
+            System.out.println(name);
+        }
     }
 }
