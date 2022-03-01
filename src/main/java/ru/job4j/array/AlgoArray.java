fa8f2df3b41b0db65fff50d036e2755bc6f9b358
ChernovSergei <sergey.v.chernov@yandex.ru>
2022-03-01 13:12:00 +0300

    1. Чтение кода и базовые ошибки [#40205]

--- src/main/java/ru/job4j/array/AlgoArray.java
+++ src/main/java/ru/job4j/array/AlgoArray.java
@@ -1,19 +1,18 @@
 package ru.job4j.array;
 
 public class AlgoArray {
+    public static int[] swapArrayElements(int first, int second, int[] array) {
+        int variable = array[first];
+        array[first] = array[second];
+        array[second] = variable;
+        return array;
+    }
+
     public static void main(String[] args) {
         int[] array = new int[]{5, 3, 2, 1, 4};
-        int temp = array[0];
-        array[0] = array[3];
-        array[3] = temp;
-
-        temp = array[4];
-        array[4] = array[3];
-        array[3] = temp;
-        temp = array[2];
-        array[2] = array[1];
-        array[1] = temp;
-
+        array = swapArrayElements(0, 3, array);
+        array = swapArrayElements(3, 4, array);
+        array = swapArrayElements(1, 2, array);
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + ", ");
         }
