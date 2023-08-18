02bed33cb811dc4827c04aa22d5f0d0d0a73f254
Sergei Chernov <sergey.v.chernov@yandex.ru>
2023-08-18 10:12:55 +0600

    2.0. FileOutputStream [#252488]

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -1,9 +1,16 @@
 package ru.job4j;
 
+import java.io.FileOutputStream;
+
 public class Multiple {
     public static void main(String[] args) {
-        for (int i = 2; i < 10; i++) {
-            System.out.println("1 * " + i + " = " + i);
+        try (FileOutputStream out = new FileOutputStream("data/dataResult.txt")) {
+            for (int i = 2; i < 10; i++) {
+                out.write(("1 * " + i + " = " + i).getBytes());
+                out.write(System.lineSeparator().getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
